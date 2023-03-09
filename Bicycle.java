57a2fe9b88ed4dfc20ef72668ea53c6a7878db32
abnaemi <ab.naemi@gmail.com>
2023-03-09 17:21:10 +0100

    Bonus Scanner in Progress

--- Bicycle.java
+++ Bicycle.java
@@ -1,6 +1,10 @@
 public class Bicycle extends Vehicle {
 
 
+    public void brake(){
+        this.velocity = 0;
+        System.out.println("Car stopped");
+    }
     @Override
     public int accelerate(int kmh) {
         if (velocity+kmh > 35) throw new IllegalStateException("Limit reached");
--- Main.java
+++ Main.java
@@ -1,15 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
             Bicycle bike = new Bicycle();
+        Scanner scanner = new Scanner(System.in);
+
+
+        do{
+        System.out.println("Do you wanna brake or speed?");
+        String input = scanner.next();
+        if (input.equals("brake")){
+            bike.brake();
+        } else {
             bike.accelerate(5);
-            bike.accelerate(5);
-        bike.accelerate(5);
-        bike.accelerate(5);
-        bike.accelerate(5);
-        bike.accelerate(5);
-        bike.accelerate(5);
-        bike.accelerate(5);
+        }} while (bike.velocity < 35 || bike.velocity >= 0);
+
+
+
+
 
 
 
